83e52c22978769aa1d9e4dd110d55cd7103c1f53
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2024-03-24 16:52:26 -0300

    Entendendo como atualizar e voltar versão de banco de dados

--- flutter_sqlite_example/lib/database/database_sqlite.dart
+++ flutter_sqlite_example/lib/database/database_sqlite.dart
@@ -8,7 +8,12 @@ class DatabaseSqLite {
 
     await openDatabase(
       databaseFinalPath,
-      version: 1,
+      version: 2,
+      // Chamado a cada conexão ao banco de dados
+      onConfigure: (db) async {
+        // Habilitando as foreignKeys do banco de dados
+        await db.execute('PRAGMA foreign_keys = ON');
+      },
       // Chamado somente no momento de criação do banco de dados, primeira vez que carrega o app
       onCreate: (Database db, int version) {
         final batch = db.batch();
@@ -19,13 +24,42 @@ class DatabaseSqLite {
             nome varchar(200)
           )
         ''');
+        batch.execute('''
+          create table produto(
+            id Integer primary key autoincrement,
+            nome varchar(200)
+          )
+        ''');
 
         batch.commit();
       },
       // Chamada sempre que houver uma alteração no version, incrementa (1 -> 2)
-      onUpgrade: (Database db, int oldVersion, int version) {},
+      onUpgrade: (Database db, int oldVersion, int version) {
+        final batch = db.batch();
+
+        if (oldVersion == 1) {
+          batch.execute('''
+            create table produto(
+              id Integer primary key autoincrement,
+              nome varchar(200)
+            )
+          ''');
+        }
+
+        batch.commit();
+      },
       // Chamada sempre que houver uma alteração no version, decrementa (3 -> 2)
-      onDowngrade: (Database db, int oldVersion, int version) {},
+      onDowngrade: (Database db, int oldVersion, int version) {
+        final batch = db.batch();
+
+        if (oldVersion == 3) {
+          batch.execute('''
+            drop table categoria
+          ''');
+        }
+
+        batch.commit();
+      },
     );
   }
 }
